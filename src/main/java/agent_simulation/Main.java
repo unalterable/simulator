3e8264d3268f7e1e7822829b17199e3551393fb8
Oliver Diamond <diamond.oliver@gmail.com>
2016-12-20 23:56:05 +0000

    better markdown

--- src/main/java/agent_simulation/Main.java
+++ src/main/java/agent_simulation/Main.java
@@ -1,14 +1,14 @@
 package agent_simulation;
 
+import simulator_platform.ISimulatorSetRules;
 import simulator_platform.records.CSVImporter;
 import simulator_platform.records.IImporter;
 import simulator_platform.simulator.ISimulatorSet;
-import simulator_platform.ISimulatorSetRules;
 import simulator_platform.simulator.SimulatorSet;
 
 public class Main {
 
-    public void main() throws Exception {
+    public static void main(String[] args) throws Exception {
         IImporter importer = new CSVImporter("data/agent_data.csv", new AgentMap());
         ISimulatorSetRules experimentRules = new AgentSimulationSetRules(importer);
         ISimulatorSet experiment = new SimulatorSet(experimentRules);
